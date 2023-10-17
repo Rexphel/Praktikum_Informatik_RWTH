a0f489ea79011b66ea6bf77ef70ee47bdf41fac4
Rexphel <philiprexroth@gmail.com>
2023-10-18 00:59:08 +0200

    Worked on 4.2.4 WIP Debugger stuff

--- Aufgabenblock_1/src/main.cpp
+++ Aufgabenblock_1/src/main.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
+#include <vector>
 #include "Fahrzeug.h"
 
 int main() {
@@ -30,4 +32,25 @@ void vAufgabe_1(){
 	for (auto &p : {Mercedes, Mini, Empty}){
 		delete p;
 	};
+
+	std::vector<std::unique_ptr<Fahrzeug>> u_vFahrzeuge;																//Only able to store unique_ptr<Fahrzeuge>... like declared?
+	std::vector<std::shared_ptr<Fahrzeug>> s_vFahrzeuge;																//Only able to again store shared_ptr<Fahrzeuge> due to the declaration.
+
+	std::unique_ptr<Fahrzeug> Benz = std::make_unique<Fahrzeug>("AMG");
+	std::unique_ptr<Fahrzeug> LWK = std::make_unique<Fahrzeug>("Axor");
+	u_vFahrzeuge.push_back(move(Benz));																					//Move declares the change of ownership.
+	u_vFahrzeuge.push_back(move(LWK));
+	std::shared_ptr<Fahrzeug> Tesla = std::make_shared<Fahrzeug>("Model Y");
+	std::shared_ptr<Fahrzeug> Fiat = std::make_shared<Fahrzeug>("500");
+	s_vFahrzeuge.push_back(Tesla);																						//No move needed, due to shared_ptr being able to have multiple references/"owners"
+	s_vFahrzeuge.push_back(Fiat);
+
+	std::cout << Tesla.use_count() << std::endl;
+	std::shared_ptr<std::shared_ptr<Fahrzeug>> ptr_Tesla = std::make_shared<std::shared_ptr<Fahrzeug>>(Tesla);
+	std::cout << Tesla.use_count() << std::endl;
+
+	std::unique_ptr<std::unique_ptr<Fahrzeug>> ptr_Benz = std::make_unique<std::unique_ptr<Fahrzeug>>(move(Benz));		//Required to move the unique_ptr due to inheritance rules.
+
+	u_vFahrzeuge.clear();
+
 }
