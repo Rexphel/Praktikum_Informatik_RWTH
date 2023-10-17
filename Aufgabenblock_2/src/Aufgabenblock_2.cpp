5140985e98b1ac9d34c406cf3cd2d7fd60e129b0
Rexphel <philiprexroth@gmail.com>
2023-10-17 22:15:20 +0200

    Added Aufgabenblock 2 and 3. Updated .gitignore

---
+++ Aufgabenblock_2/src/Aufgabenblock_2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Aufgabenblock_2.cpp
+// Author      : Philip Rexroth
+// Version     :
+// Copyright   : Copyright (c) 2023 Philip Rexroth
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
