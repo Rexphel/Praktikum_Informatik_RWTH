755975395aa5fa4f9cd508583101dd05941dbe10
philiprexroth <philip.rexroth@rwth-aachen.de>
2024-01-13 16:47:48 +0100

    Merged with Laptop

--- Aufgabenblock_2/src/GlobalIncludes.hpp
+++ Aufgabenblock_2/src/GlobalIncludes.hpp
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Rexphel/Praktikum_Informatik_RWTH.git
 //============================================================================
 // Name        : GlobalIncludes.h
 // Author      : Philip Rexroth
@@ -37,3 +38,45 @@ double randDouble(int lower, int upper);
 void console_clear_screen(void);
 
 #endif /* GLOBALINCLUDES_HPP_ */
+=======
+//============================================================================
+// Name        : GlobalIncludes.h
+// Author      : Philip Rexroth
+// Version     : 0.6
+// Copyright   : Copyright (c) 2023 Philip Rexroth
+// Description : Standard Class for Vehicles in Vehicle Sim 1923
+//============================================================================
+
+
+#ifndef GLOBALINCLUDES_HPP_
+#define GLOBALINCLUDES_HPP_
+
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <random>
+
+#include "Objects/Simulationsobjekt.hpp"
+
+#include "Objects/Roads/headers/Weg.hpp"
+#include "Objects/Roads/headers/Tempolimit.hpp"
+
+#include "Objects/Vehicle/headers/Fahrrad.hpp"
+#include "Objects/Vehicle/headers/Fahrzeug.hpp"
+#include "Objects/Vehicle/headers/PKW.hpp"
+#include "Objects/Vehicle/headers/Verhalten.hpp"
+
+#define WINDOWS 1
+#define LINUX 0
+#define DEBUG TRUE
+
+std::string setStringlength(long long unsigned i, std::string s);																//Function to set a given string to any given size
+
+double randDouble(int lower, int upper);
+
+void console_clear_screen(void);
+
+#endif /* GLOBALINCLUDES_HPP_ */
+>>>>>>> 75c89d8 Merged with Laptop
